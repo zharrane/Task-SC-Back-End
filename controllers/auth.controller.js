6679269a1e2ed779ad4e9d11d9136645312704c6
zharrane <zakariaharrane2@gmail.com>
2021-11-24 23:38:11 +0100

    fix capital letters

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -7,9 +7,11 @@ exports.signin = (req, res) => {
   if (header.toString() !== "application/json")
     return res.status(400).send({ message: "Missing headers" })
 
-  const { username, password } = req.body
+  let { username, password } = req.body
   if (!username || !password)
     return res.status(400).send({ message: "Missing credentials" })
+  username = username.toString().toLowerCase()
+  password = password.toString().toLowerCase()
   const user = helpers.findOneByUsername(Users, username)
   try {
     if (!user) return res.status(404).send({ message: "User Not found." })
